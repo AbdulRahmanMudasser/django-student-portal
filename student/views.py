0302fe31a8ff50ff82123cb3933f6907db3d7d28
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-03-12 01:27:35 +0500

    defining basic views, urls, base template

--- student/views.py
+++ student/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add Student View
+def student_add(request):
+    # Render Add Student Template
+    return render(request, 'students/add-student.html')
+
+# All Student Details View
+def students_list(request):
+    # Render Student Details Template
+    return render(request, 'students/students.html')
+
+# Edit Student View
+def student_edit(request):
+    # Render Edit Student Template
+    return render(request, 'students/edit_student.html')
+
+# Student Details View
+def student_details(request):
+    # Render Student Details Template
+    return render(request, 'students/student-details.html')
